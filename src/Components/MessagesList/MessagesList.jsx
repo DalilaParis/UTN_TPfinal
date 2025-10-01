a9ee907cc93a551a7581cd4630eecd24d6313de7
DalilaParis <paris.dalila2007@gmail.com>
2025-10-01 10:31:27 -0300

    solucion de problemas

--- src/Components/MessagesList/MessagesList.jsx
+++ src/Components/MessagesList/MessagesList.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Message from '../MessageProp/Message.js'
+import Message from '../MessageProp/MessageProp.jsx'
 import './messagesList.css'
 
 const MessagesList = (props) => {
