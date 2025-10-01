235bf5ebe222356f7214ebdfdc95893c7bf1c118
DalilaParis <paris.dalila2007@gmail.com>
2025-10-01 12:45:31 -0300

    Añado los distintos contactos

--- src/Components/ContactList/ContactList.jsx
+++ src/Components/ContactList/ContactList.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { getAllContacts } from '../../services/contactService'
 import { Link } from 'react-router'
 import { ContactListContext } from '../../Context/ContactListContext'
+import "./ContactList.css"
 
 const ContactList = () => {
     //useContext es un hook nativo de react que me permite consumir contextos
@@ -29,10 +30,14 @@ const ContactList = () => {
 const ContactItem = (props) => {
     const contact = props.contact
     return (
-        <Link to={'/contacto/' + contact.id} >
-            <h2>{contact.name}</h2>
-            <span>En linea: {contact.is_connected ? 'Si' : 'No'}</span>
-            <img src={contact.profile_img} width={'150px'} />
+        <Link to={'/contacto/' + contact.id} class="contact">
+            <div class="profile-pic">
+                <img src={contact.profile_img} class="profile-pic_image" />
+            </div>
+            <div class="contact-info">
+                <h2 class="contact-info_name">{contact.name}</h2>
+                <span class="contact-info_online">En linea: {contact.is_connected ? 'Si' : 'No'}</span>
+            </div>
             <hr />
         </Link>
     )
--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,39 +1,63 @@
 const contacts = [
     {
         id: 1,
-        name: 'pepito',
+        name: 'Sans',
         last_time_connected: '16 de octubre del 2022',
         is_connected: false,
-        profile_img: 'https://cdn.mos.cms.futurecdn.net/nyU6UyNw4B4QVLj69n5hbe-650-80.jpg.webp',
-        state: 'Tu espera ese dia y cuando llegue ese dia, ese sera el dia',
+        profile_img: 'https://www.pngkey.com/png/full/175-1759856_sans-face-sans-pixel-art.png',
+        state: 'eeeeeeee',
         messages: [
             {
                 id: 1,
                 author: 'YO',
-                content: "Que tal!",
-                timestamp: '16:17'
-            }
-        ]
-    },
-    {
-        id: 2,
-        name: 'maria',
-        last_time_connected: 'ahora',
-        is_connected: true,
-        profile_img: 'https://cdn.mos.cms.futurecdn.net/nyU6UyNw4B4QVLj69n5hbe-650-80.jpg.webp',
-        state: 'Tu espera ese dia y cuando llegue ese dia, ese sera el dia',
-        messages: [
-            {
-                id: 1,
-                author: 'YO',
-                content: "Hola maria, todo bien?",
+                content: "Que haces loco?",
                 timestamp: '16:17'
             },
             {
                 id: 2,
-                author: 'maria',
-                content: "si.",
+                author: 'Sans',
+                content: "Eee ee e",
+                timestamp: '17:23'
+            },
+            {
+                id: 2,
+                author: 'Sans',
+                content: "Soy trans",
+                timestamp: '17:23'
+            },
+            {
+                id: 2,
+                author: 'Sans',
+                content: "Sans",
+                timestamp: '17:24'
+            },
+        ]
+    },
+    {
+        id: 2,
+        name: 'Edwin',
+        last_time_connected: 'ahora',
+        is_connected: true,
+        profile_img: 'https://i.kym-cdn.com/photos/images/newsfeed/003/020/413/743.png',
+        state: 'Hice el mimic',
+        messages: [
+            {
+                id: 1,
+                author: 'YO',
+                content: "Hola, quien sos?",
+                timestamp: '16:17'
+            },
+            {
+                id: 2,
+                author: 'Edwin',
+                content: "Mi nombre es Edwin, hice al Mimic.",
                 timestamp: '16:18'
+            },
+            {
+                id: 2,
+                author: 'Edwin',
+                content: "Fue muy dificil juntar las piezas...",
+                timestamp: '16:19'
             }
         ]
     }
