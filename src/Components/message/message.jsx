a978bb39626ba2b6ca92e9b4d354b95b56860129
DalilaParis <paris.dalila2007@gmail.com>
2025-10-01 10:04:11 -0300

    repositorio del proyecto final

---
+++ src/Components/message/message.jsx
@@ -0,0 +1,12 @@
+function Message(propiedades){
+    return(
+        <div>
+            <span>{propiedades.author}</span>
+            <p>{propiedades.content}</p>
+            <span>{propiedades.timestamp}</span>
+            <hr />
+        </div>
+    )
+}
+
+export default Message
