3958c8e71b448314945480be58e5bf7938988b3f
DalilaParis <paris.dalila2007@gmail.com>
2025-10-07 10:32:57 -0300

    cambios minimos en el css

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -14,20 +14,6 @@ const Navbar = () => {
             <a href=""><i class="bi bi-chat-dots"></i></a>
             <a href=""><i class="bi bi-people"></i></a>
         </nav>
-        <button 
-            className='btn-menu' 
-            onClick={toggleMenu}
-        >menu</button>
-
-        {
-            is_open &&
-            <nav className='nav-wsp_mobible'>
-                <a href="">Link 1</a>
-                <a href="">Link 2</a>
-                <a href="">Link 3</a>
-                <a href="">Link 4</a>
-            </nav>
-        }
         <div class="user-interface">
             <a href=""><i class="bi bi-gear"></i></a>
             <a href=""><img src="https://i.pinimg.com/736x/67/0e/1f/670e1fc607d825c1a783f4308043be28.jpg" class="user-pfp"/></a>
